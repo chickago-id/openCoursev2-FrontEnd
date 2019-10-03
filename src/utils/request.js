f6809fa91a8afe2900f4dc8fe26fdda78b808790
akbarlaz <akbar.lazuardi@yahoo.com>
2019-10-04 05:30:13 +0700

    2019-10-04 change baseurl to hardcode for temporary, change store.dispatch, change if statement

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  baseURL: 'http://localhost:8081', // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
@@ -14,12 +14,11 @@ const service = axios.create({
 service.interceptors.request.use(
   config => {
     // do something before request is sent
-
     if (store.getters.token) {
       // let each request carry token
       // ['X-Token'] is a custom headers key
       // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+      config.headers[['Authorize']] = getToken()
     }
     return config
   },
@@ -44,9 +43,8 @@ service.interceptors.response.use(
    */
   response => {
     const res = response.data
-
     // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
+    if (res.access_token === null) {
       Message({
         message: res.message || 'Error',
         type: 'error',
@@ -61,7 +59,7 @@ service.interceptors.response.use(
           cancelButtonText: 'Cancel',
           type: 'warning'
         }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
+          store.dispatch('aresetToken').then(() => {
             location.reload()
           })
         })
@@ -71,7 +69,7 @@ service.interceptors.response.use(
       return res
     }
   },
-  error => {
+  /* error => {
     console.log('err' + error) // for debug
     Message({
       message: error.message,
@@ -79,7 +77,7 @@ service.interceptors.response.use(
       duration: 5 * 1000
     })
     return Promise.reject(error)
-  }
+  } */
 )
 
 export default service

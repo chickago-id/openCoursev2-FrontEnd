cecda183aa3b55fd81bd77dcd8e4449515d1f5ae
akbarlaz <akbar.lazuardi@yahoo.com>
2019-10-04 05:24:35 +0700

    2019-10-04 add some getters

--- src/store/getters.js
+++ src/store/getters.js
@@ -3,6 +3,8 @@ const getters = {
   device: state => state.app.device,
   token: state => state.user.token,
   avatar: state => state.user.avatar,
-  name: state => state.user.name
+  name: state => state.user.name,
+  isAuthenticated: state => !!state.user.token,
+  authStatus: state => state.user.status
 }
 export default getters

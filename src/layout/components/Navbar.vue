73d19bf797c831ce792715ea6b0f276a525f60f8
akbarlaz <akbar.lazuardi@yahoo.com>
2019-10-04 12:58:28 +0700

    2019-10-04 change store.dispatch

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -52,7 +52,7 @@ export default {
       this.$store.dispatch('app/toggleSideBar')
     },
     async logout() {
-      await this.$store.dispatch('logout')
+      await this.$store.dispatch('user/logout')
       this.$router.push(`/login?redirect=${this.$route.fullPath}`)
     }
   }

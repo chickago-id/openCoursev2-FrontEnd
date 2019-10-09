a08bdf10d49b83d1756c9916bdd7ccdfc5c4f4db
abror <wf.abror@gmail.com>
2019-10-10 00:02:23 +0700

    2019-10-10 update Navbar

---
+++ src/views/profile/components/Activity.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="user-activity">
+    <div class="post">
+      <div class="user">
+        <span class="username text-muted">Batch</span>
+        <span class="description">Batch yang diambil</span>
+      </div>
+      <p>
+        Keterangan batch yang diambil
+      </p>
+      <div class="user">
+        <span class="username text-muted">Jadwal Kelas</span>
+        <span class="description">Jadwal Kelas yang diambil</span>
+      </div>
+      <p>
+        Keterangan Jadwal kelas yang diambil
+      </p>
+      <div class="user">
+        <span class="username text-muted">Jadwal Kelas Hari ini</span>
+        <span class="description">Jadwal Kelas Hari ini</span>
+      </div>
+      <p>
+        Keterangan Jadwal hari ini jika tidak ada kelas maka kosong
+      </p>
+      <ul class="list-inline">
+        <li>
+          <span class="link-black text-sm">
+            <i class="el-icon-share" />
+            Share
+          </span>
+        </li>
+        <li>
+          <span class="link-black text-sm">
+            <svg-icon icon-class="like" />
+            Like
+          </span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+const avatarPrefix = '?imageView2/1/w/80/h/80'
+const carouselPrefix = '?imageView2/2/h/440'
+
+export default {
+  data() {
+    return {
+      carouselImages: [
+        'https://wpimg.wallstcn.com/9679ffb0-9e0b-4451-9916-e21992218054.jpg',
+        'https://wpimg.wallstcn.com/bcce3734-0837-4b9f-9261-351ef384f75a.jpg',
+        'https://wpimg.wallstcn.com/d1d7b033-d75e-4cd6-ae39-fcd5f1c0a7c5.jpg',
+        'https://wpimg.wallstcn.com/50530061-851b-4ca5-9dc5-2fead928a939.jpg'
+      ],
+      avatarPrefix,
+      carouselPrefix
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.user-activity {
+  .user{
+    .description {
+      display: block;
+      padding: 2px 0;
+    }
+    .username{
+      font-size: 16px;
+      color: #000;
+    }
+
+    span {
+      font-weight: 500;
+      font-size: 12px;
+    }
+  }
+  .user-block {
+
+    .username,
+    .description {
+      display: block;
+      margin-left: 50px;
+      padding: 2px 0;
+    }
+
+    .username{
+      font-size: 16px;
+      color: #000;
+    }
+
+    :after {
+      clear: both;
+    }
+
+    .img-circle {
+      border-radius: 50%;
+      width: 40px;
+      height: 40px;
+      float: left;
+    }
+
+    span {
+      font-weight: 500;
+      font-size: 12px;
+    }
+  }
+
+  .post {
+    font-size: 14px;
+    border-bottom: 1px solid #d2d6de;
+    margin-bottom: 15px;
+    padding-bottom: 15px;
+    color: #666;
+
+    .image {
+      width: 100%;
+      height: 100%;
+
+    }
+
+    .user-images {
+      padding-top: 20px;
+    }
+  }
+
+  .list-inline {
+    padding-left: 0;
+    margin-left: -5px;
+    list-style: none;
+
+    li {
+      display: inline-block;
+      padding-right: 5px;
+      padding-left: 5px;
+      font-size: 13px;
+    }
+
+    .link-black {
+
+      &:hover,
+      &:focus {
+        color: #999;
+      }
+    }
+  }
+
+}
+
+.box-center {
+  margin: 0 auto;
+  display: table;
+}
+
+.text-muted {
+  color: #777;
+}
+</style>

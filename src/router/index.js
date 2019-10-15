58f458a54b5ff0a2ade9490239e3ebebb135cc99
Supi Core <supi.core@gmail.com>
2019-10-15 10:27:42 +0700

    2019-10-15 | Add list menu master data Day Name and Jadwal

--- src/router/index.js
+++ src/router/index.js
@@ -78,6 +78,11 @@ export const constantRoutes = [
         meta: { title: "Daftar Materi" }
       },
       {
+        path: "jadwal",
+        component: () => import("@/views/akademik/jadwal/index"),
+        meta: { title: "Jadwal" }
+      },
+      {
         path: "nilaisiswa",
         component: () => import("@/views/akademik/nilaisiswa/index"),
         meta: { title: "Nilai Siswa" },
@@ -207,6 +212,11 @@ export const constantRoutes = [
         meta: { title: "Master Sesi" }
       },
       {
+        path: "day",
+        component: () => import("@/views/masterdata/hari/index"),
+        meta: { title: "Daftar Hari" }
+      },
+      {
         path: "tahunakademik",
         component: () => import("@/views/masterdata/tahunakademik/index"),
         meta: { title: "Master Tahun Akademik" }
@@ -224,11 +234,11 @@ export const constantRoutes = [
       icon: "nested"
     },
     children: [
-      // {
-      //   path: 'profilinstitusi',
-      //   component: () => import('@/views/manajemeninstitusi/profilinstitusi/index'),
-      //   meta: { title: 'Institute Management' }
-      // },
+      {
+        path: 'profilinstitusi',
+        component: () => import('@/views/manajemeninstitusi/profilinstitusi/index'),
+        meta: { title: 'Institute Management' }
+      },
       {
         path: "listinstitusi",
         component: () =>
@@ -274,111 +284,6 @@ export const constantRoutes = [
       }
     ]
   },
-
-  // {
-  //   path: '/example',
-  //   component: Layout,
-  //   redirect: '/example/table',
-  //   name: 'Example',
-  //   meta: { title: 'Example', icon: 'example' },
-  //   children: [
-  //     {
-  //       path: 'table',
-  //       name: 'Table',
-  //       component: () => import('@/views/table/index'),
-  //       meta: { title: 'Table', icon: 'table' }
-  //     },
-  //     {
-  //       path: 'tree',
-  //       name: 'Tree',
-  //       component: () => import('@/views/tree/index'),
-  //       meta: { title: 'Tree', icon: 'tree' }
-  //     }
-  //   ]
-  // },
-
-  // {
-  //   path: '/form',
-  //   component: Layout,
-  //   children: [
-  //     {
-  //       path: 'index',
-  //       name: 'Form',
-  //       component: () => import('@/views/form/index'),
-  //       meta: { title: 'Form', icon: 'form' }
-  //     }
-  //   ]
-  // },
-
-  // {
-  //   path: '/nested',
-  //   component: Layout,
-  //   redirect: '/nested/menu1',
-  //   name: 'Nested',
-  //   meta: {
-  //     title: 'Nested',
-  //     icon: 'nested'
-  //   },
-  //   children: [
-  //     {
-  //       path: 'menu1',
-  //       component: () => import('@/views/nested/menu1/index'), // Parent router-view
-  //       name: 'Menu1',
-  //       meta: { title: 'Menu1' },
-  //       children: [
-  //         {
-  //           path: 'menu1-1',
-  //           component: () => import('@/views/nested/menu1/menu1-1'),
-  //           name: 'Menu1-1',
-  //           meta: { title: 'Menu1-1' }
-  //         },
-  //         {
-  //           path: 'menu1-2',
-  //           component: () => import('@/views/nested/menu1/menu1-2'),
-  //           name: 'Menu1-2',
-  //           meta: { title: 'Menu1-2' },
-  //           children: [
-  //             {
-  //               path: 'menu1-2-1',
-  //               component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-  //               name: 'Menu1-2-1',
-  //               meta: { title: 'Menu1-2-1' }
-  //             },
-  //             {
-  //               path: 'menu1-2-2',
-  //               component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-  //               name: 'Menu1-2-2',
-  //               meta: { title: 'Menu1-2-2' }
-  //             }
-  //           ]
-  //         },
-  //         {
-  //           path: 'menu1-3',
-  //           component: () => import('@/views/nested/menu1/menu1-3'),
-  //           name: 'Menu1-3',
-  //           meta: { title: 'Menu1-3' }
-  //         }
-  //       ]
-  //     },
-  //     {
-  //       path: 'menu2',
-  //       component: () => import('@/views/nested/menu2/index'),
-  //       meta: { title: 'menu2' }
-  //     }
-  //   ]
-  // },
-
-  // {
-  //   path: 'external-link',
-  //   component: Layout,
-  //   children: [
-  //     {
-  //       path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-  //       meta: { title: 'External Link', icon: 'link' }
-  //     }
-  //   ]
-  // },
-  
   {
     path: '/pdf/presensisiswa',
     component: () => import('@/views/pdf/presensisiswa/index'),
@@ -400,7 +305,7 @@ export const constantRoutes = [
   {
     path: "/report",
     component: Layout,
-    redirect: "/masterdata/menu1",
+    redirect: "/report/menu1",
     name: "report",
     meta: {
       title: "Report",

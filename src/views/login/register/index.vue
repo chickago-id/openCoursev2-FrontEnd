e1f64cb9bd120c2f5f66ecdd92b7b7550d3794b7
kaijack <jakkse7en@gmail.com>
2019-10-19 11:13:07 -0400

    2019/10/19 pendaftaran sudah bisa and add validate form

--- src/views/login/register/index.vue
+++ src/views/login/register/index.vue
@@ -1,124 +1,128 @@
 <template>
-  <div class="contai">
-    <div role="group">
-
-      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on"
-        label-position="left">
-
-        <div>
-          <h2>Create your Applications Account</h2>
-          <p>to continue to Application</p>
-        </div>
-
-        <el-form-item prop="username">
-          <label class="inputkeren">
-            <input type="text" placeholder=" " ref="username" v-model="akun.username" name="username" tabindex="1"
-              auto-complete="on">
-            <span>Username</span>
-          </label>
+  <div style="padding:0px 20px 30px 30px;">
+     <div class="title-container">
+        <h1 style="text-align: center;">Sign Up</h1>
+        <h5 style="text-align: center;">to continue to application</h5>
+      </div>
+     <el-form  :model="form" status-icon  ref="form" class="demo-form" :rules="regisVal">
+       <el-form-item prop="username">
+           <label class="inputkeren">
+             <input type="text" placeholder=" " v-model="form.username" autocomplete="off">
+             <span>Username</span>
+           </label>            
         </el-form-item>
-        <el-form-item prop="email">
-          <label class="inputkeren">
-            <input placeholder=" " id="email" v-model="akun.email" type="email">
+        <el-form-item required="" prop="email" >
+           <label class="inputkeren">
+            <input type="email" placeholder=" " v-model="form.email" autocomplete="off">
             <span>Email</span>
           </label>
         </el-form-item>
-        <el-form-item prop="password">
-          <label class="inputkeren">
-            <input id="password1" :key="passwordType" ref="password" v-model="akun.password1" :type="passwordType"
-              placeholder=" " name="password1" tabindex="2" auto-complete="on" @keyup.enter.native="handleLogin">
+        <el-form-item  prop="password">
+           <label class="inputkeren">
+            <input :key="passwordType" :type="passwordType" placeholder=" " v-model="form.password" autocomplete="off"  >
             <span>Password</span>
-            <span class="show-pwd" @click="showPwd">
-              <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-            </span>
+             <span class="show-pwd" @click="showPwd">
+                      <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+              </span>
           </label>
         </el-form-item>
-        <el-form-item prop="password">
-          <label class="inputkeren">
-            <input id="password2" :key="passwordType" ref="password" v-model="akun.password2" :type="passwordType"
-              placeholder=" " name="password2" tabindex="2" auto-complete="on" @keyup.enter.native="handleLogin">
-            <span>Password</span>
+        <el-form-item  prop="password2">
+           <label class="inputkeren">
+            <input :key="passwordType" :type="passwordType" placeholder=" " v-model="form.password2" autocomplete="off"
+            >
+            <span>Re-Password</span>
           </label>
         </el-form-item>
-        <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;"
-          @click.native.prevent="handleLogin">Sign Up</el-button>
-
-      </el-form>
-    </div>
+      </el-form>  
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="clearData">Cancel</el-button>
+        <el-button type="primary" @click="submitForm('form')">Confirm</el-button>
+      </span>
   </div>
-  <!-- kai -->
 </template>
 
 <script>
-  import axios from 'axios'
-  import {
-    validUsername
-  } from '@/utils/validate'
+import { mapGetters } from "vuex";
+import axios from "axios";
 
-
-  export default {
-
-    name: 'Login',
-    data() {
-
-      const validateUsername = (rule, value, callback) => {
-        if (!validUsername(value)) {
-          callback(new Error('Please enter the correct user name'))
+export default {
+  computed: {
+    ...mapGetters(["token", "username", "roles"])
+  },
+  data() {
+ var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('Please input the password'));
+        } else if (value.length < 6) {
+          callback(new Error('The password must more than 6 digits'))
         } else {
-          callback()
+          if (this.form.password2 !== '') {
+            this.$refs.form.validateField('password2');
+          }
+          callback();
         }
-      }
-      const validatePassword = (rule, value, callback) => {
-        if (value.length < 6) {
-          callback(new Error('The password can not be less than 6 digits'))
+      };
+      var validatePass2 = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('Please input the password again'));
+        } else if (value !== this.form.password) {
+          callback(new Error('Two inputs don\'t match!'));
         } else {
-          callback()
+          callback();
         }
-      }
-      return {
-        activeName: 'first',
-        password2: '',
-        loginForm: {
-          username: '',
-          password: ''
+      };
+    return {
+      list: null,
+      listLoading: true,
+      listData: [],
+      password2: "",
+      form: {
+        username: "",
+        email: "",
+        password: ""
+      },
+      regisVal: {
+            username: [
+            { required: true, message: 'Please input username', trigger: 'blur' },
+            { min: 3, max: 8, message: 'Length should be 3 to 8', trigger: 'blur' }
+          ],
+          email: [
+            { required: true, message: 'Please input email address', trigger: 'blur' },
+            { type: 'email', message: 'Please input correct email address', trigger: ['blur', 'change'] }
+          ],
+          password: [
+            { required: true, validator: validatePass, trigger: 'blur' }
+          ],
+          password2: [
+            { required: true, validator: validatePass2, trigger: 'blur'}
+          ],
         },
-        akun: {
-          domains: [{
-            key: 1,
-            value: ''
-          }],
-          email: '',
-          username: '',
-          password: ''
-        },
+      passwordType: 'password',
+    };
+    
+    
+  },
+  created() {
+    this.getData();
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
 
-
-        loginRules: {
-          username: [{
-            required: true,
-            trigger: 'blur' /* , validator: validateUsername */
-          }],
-          password: [{
-            required: true,
-            trigger: 'blur',
-            validator: validatePassword
-          }]
-        },
-        loading: false,
-        passwordType: 'password',
-        redirect: undefined
-      }
-    },
-    watch: {
-      $route: {
-        handler: function (route) {
-          this.redirect = route.query && route.query.redirect
-        },
-        immediate: true
-      }
-    },
-    methods: {
-      handleClick(tab, event) {
+      submitForm(form) {
+        this.$refs[form].validate((valid) => {
+          if (valid) {
+            this.addData();
+            // this.successNotif();
+          } else {
+            this.gagalNotif();
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+     handleClick(tab, event) {
         console.log(tab, event);
       },
       showPwd() {
@@ -131,109 +135,86 @@
           this.$refs.password.focus()
         })
       },
-      handleLogin() {
-        this.loading = true
-        this.$refs.loginForm.validate(valid => {
-          if (valid) {
-            let username = this.loginForm.username
-            let password = this.loginForm.password
-            th
-            is.$store.dispatch('user/login', {
-                username,
-                password
-              })
-              .then(() => this.$router.push('/'))
-              .catch(err => console.log(err))
-            this.loading = false
-          } else {
-            this.loading = false
-            console.log('Anda gagal sign in')
-            return false
-          }
-        })
+    successNotif() {
+      const h = this.$createElement;
+      this.$notify({
+        title: "Success",
+        message: h(
+          "i",
+          { style: "color: teal" },
+          "Create Account Success"
+        ),
+        type: "success",
+        showClose: false,
+        duration: 2000
+      });
+    },
+     gagalNotif() {
+      const h = this.$createElement;
+      this.$notify({
+        title: "Failed",
+        message: h(
+          "i",
+          { style: "color: teal" },
+          "lengakapi dengan benar bray"
+        ),
+        type: "info",
+        showClose: false,
+        duration: 2000
+      });
+    },
+    getData() {
+      this.listLoading = true;
+      axios.get("http://localhost:8081/buat-akun").then(response => {
+        this.listData = response.data;
+        this.listLoading = false;
+      });
+    },
+    clearData() {
+      this.form.username = "";
+      this.form.email = "";
+      this.form.password = "";
+      this.form.password2 = "";
+    },
+    addData() {
+      const token = "Bearer " + localStorage.getItem("token");
+      const auth = {
+        Authorization: token,
+        "Content-Type": "application/json"
+      };
+      console.log(token);
+      if (this.form.username != "" && this.form.email != "" && this.form.password != "") {
+        axios
+          .post(
+            process.env.VUE_APP_BASE_API +
+              "/buat-akun", 
+            this.form,
+            { headers: auth }
+          )
+          .then(data => {
+            this.getData();
+            this.successNotif();
+            this.clearData();
+            this.dialogFormVisible = false;
+          });
+      } else {
+        this.gagalNotif();
+          
       }
     }
+       
   }
-
+};
 </script>
-
-<style lang="scss" scoped>
-  /* 修复input 背景不协调 和光标变色 */
-  /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
-
-  $bg:#283443;
-  $light_gray:#fff;
-  $cursor: #fff;
-
-  @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-    .login-container .el-input input {
-      color: $cursor;
-    }
-  }
-
-  .el-tab-pane label {
-    color: white;
-  }
-
-  .el-card {
-    width: 440px;
-    height: auto;
-    padding: 20px;
-    background: linear-gradient(rgb(255, 255, 255), #283443, #283443, #283443, #283443, #283443);
-    position: fixed;
-    top: 20%;
-    left: 50%;
-    margin-top: -120px;
-    margin-left: -220px;
-  }
-
-  /* reset element-ui css */
-  .container {
-    padding: 8%;
-
-    .el-input {
-      display: inline-block;
-      height: 35px;
-      width: 55%;
-
-      input {
-        background: transparent;
-        border: 0px;
-        -webkit-appearance: none;
-        border-radius: 0px;
-        padding: 12px 5px 12px 15px;
-        color: $light_gray;
-        height: 47px;
-        caret-color: $cursor;
-
-        &:-webkit-autofill {
-          box-shadow: 0 0 0px 1000px $bg inset !important;
-          -webkit-text-fill-color: $cursor !important;
-        }
-      }
-    }
-
-    .el-form-item {
-      // border: 1px solid rgba(255, 255, 255, 0.1);
-      // background: rgba(0, 0, 0, 0.1);
-      // border-radius: 5px;
-      // color: #454545;
-    }
-  }
-
-</style>
-
 <style lang="scss" scoped>
   $bg: transparent;
   $dark_gray:#889aa4;
   $light_gray:#eee;
-
   .container {
     min-height: 100%;
     width: 100%;
     background-color: $bg;
     overflow: hidden;
-
     .form {
       position: relative;
       width: 520px;
@@ -242,30 +223,18 @@
       margin: 0 auto;
       overflow: hidden;
     }
-
     .tips {
       font-size: 14px;
       color: #fff;
       margin-bottom: 10px;
-
       span {
         &:first-of-type {
           margin-right: 16px;
         }
       }
     }
-
-    .svg-container {
-      padding: 6px 5px 6px 15px;
-      color: $dark_gray;
-      vertical-align: middle;
-      width: 30px;
-      display: inline-block;
-    }
-
     .title-container {
       position: relative;
-
       .title {
         font-size: 26px;
         color: $light_gray;
@@ -274,7 +243,6 @@
         font-weight: bold;
       }
     }
-
     .show-pwd {
       position: absolute;
       right: 10px;
@@ -285,5 +253,4 @@
       user-select: none;
     }
   }
-
 </style>

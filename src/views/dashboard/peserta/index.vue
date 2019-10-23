f31b9f8f3a113483cadb8b49bb8610886db69bbd
akbarlaz <akbar.lazuardi@yahoo.com>
2019-10-23 13:29:51 +0700

    2019-10-23 add dashboard peserta

---
+++ src/views/dashboard/peserta/index.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="dashboard-editor-container">
+      
+      
+      <h1>Peserta</h1>
+      {{this.$store.state.permission.routes}}asdsa
+      </div>
+</template>
+
+<script>
+
+export default {
+    name: 'DashboardPeserta',
+    components: {
+      
+    },
+    data() {
+      return {
+      //  lineChartData: lineChartData.siswa
+      }
+    },
+    methods: {
+      
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard-editor-container {
+  padding: 12px;
+  background-color: rgb(240, 242, 245);
+  position: relative;
+
+  .github-corner {
+    position: absolute;
+    top: 0px;
+    border: 0;
+    right: 0;
+  }
+
+  .chart-wrapper {
+    background: #fff;
+    padding: 16px 16px 0;
+    margin-bottom: 32px;
+    font-family:Arial, Helvetica, sans-serif
+  }
+}
+
+@media (max-width:1024px) {
+  .chart-wrapper {
+    padding: 8px;
+  }
+}
+</style>

6863d903842de3bc10a2831c5b42d91aaa8e8a58
abror <wf.abror@gmail.com>
2019-10-23 15:25:13 +0700

    2019-10-23 change category for see rekap jumlah kelas

--- src/views/dashboard/admin/components/BarChart.vue
+++ src/views/dashboard/admin/components/BarChart.vue
@@ -6,6 +6,7 @@
 import echarts from 'echarts'
 require('echarts/theme/macarons') // echarts theme
 import resize from './mixins/resize'
+import { relative } from 'path'
 
 export default {
   mixins: [resize],
@@ -26,6 +27,10 @@ export default {
       type: Boolean,
       default: true
     },
+    // position:{
+    //   type:Object,
+    //   default:relative
+    // },
     chartData: {
       type: Object,
       required: true
@@ -64,7 +69,7 @@ export default {
     setOptions({ expectedData, actualData } = {}) {
       this.chart.setOption({
          title: {
-        text: 'Jurusan',
+        text: 'Rekap Jumlah Kelas Berdasarkan Jenis Kelas',
         subtext: ''
     },
     tooltip: {
@@ -80,7 +85,7 @@ export default {
         }
     },
     legend: {
-        data: ['2017', '2018']
+        data: ['Intensif', 'Privat', 'Sertifikasi', 'Rombel', 'Profesi']
     },
     grid: {
         left: '3%',
@@ -89,23 +94,39 @@ export default {
         containLabel: true
     },
     xAxis: {
+        type: 'category',
+        data: ['Januari','Februari','Maret','April','Mei','Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
+        
+    },
+    yAxis: {
         type: 'value',
         boundaryGap: [0, 0.01]
     },
-    yAxis: {
-        type: 'category',
-        data: ['Vue','JS','Design','Office','Accounting','Web Programming']
-    },
     series: [
         {
-            name: '2017',
+            name: 'Intensif',
             type: 'bar',
-            data: [18203, 23489, 29034, 104970, 131744, 630230]
+            data: [6, 4, 3, 5, 0, 5, 3, 4, 0, 0, 0, 0]
         },
         {
-            name: '2018',
+            name: 'Privat',
             type: 'bar',
-            data: [19325, 23438, 31000, 121594, 134141, 681807]
+            data: [6, 12, 7, 3, 10, 6, 14, 10, 0, 0, 0, 0]
+        },
+        {
+            name: 'Sertifikasi',
+            type: 'bar',
+            data: [2, 2, 1, 1, 0, 1, 5, 0, 0, 0, 0, 0]
+        },
+        {
+            name: 'Rombel',
+            type: 'bar',
+            data: [0, 1, 3, 0, 0, 1, 1, 0, 0, 0, 0, 0]
+        },
+        {
+            name: 'Profesi',
+            type: 'bar',
+            data: [1, 0, 0, 1, 0, 0, 0, 0, 0, 0]
         }
     ]
       })

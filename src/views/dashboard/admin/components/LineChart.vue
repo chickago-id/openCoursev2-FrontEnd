7258eddd1afb594ec4e8c857c4c29b7b422c1616
abror <wf.abror@gmail.com>
2019-10-23 15:26:33 +0700

    2019-10-23 change category for see total vs periode

--- src/views/dashboard/admin/components/LineChart.vue
+++ src/views/dashboard/admin/components/LineChart.vue
@@ -61,17 +61,17 @@ export default {
       this.chart = echarts.init(this.$el, 'macarons')
       this.setOptions(this.chartData)
     },
-    setOptions({ expectedData, actualData } = {}) {
+    setOptions({ jumlahKelas, jumlahPeserta }   = {}) {
       this.chart.setOption({
-         title : {
-        text: 'Chart',
+        title : {
+        text: 'Total vs Peroide',
         subtext: ''
         },
         tooltip : {
             trigger: 'axis'
         },
         legend: {
-            data:['expected','actual']
+            data:['Kelas','Peserta']
         },
         toolbox: {
             show : true,
@@ -87,7 +87,7 @@ export default {
             {
                 type : 'category',
                 data : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Des']
-            }
+            },
         ],
         yAxis : [
             {
@@ -95,7 +95,7 @@ export default {
             }
         ],
         series: [{
-          name: 'expected', itemStyle: {
+          name: 'Kelas', itemStyle: {
             normal: {
               color: '#FF005A',
               lineStyle: {
@@ -106,12 +106,12 @@ export default {
           },
           smooth: true,
           type: 'line',
-          data: expectedData,
+          data: [15, 19, 14, 10, 10, 13, 23, 14, 0, 0, 0, 0],
           animationDuration: 2800,
           animationEasing: 'cubicInOut'
         },
         {
-          name: 'actual',
+          name: 'Peserta',
           smooth: true,
           type: 'line',
           itemStyle: {
@@ -126,7 +126,7 @@ export default {
               }
             }
           },
-          data: actualData,
+          data: [63, 61, 66, 27, 12, 41, 135, 24, 0, 0, 0, 0],
           animationDuration: 2800,
           animationEasing: 'quadraticOut'
         }]

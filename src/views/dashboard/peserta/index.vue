31c80cbe4e3fe7f7b22695f9a9fc68952e12cfa7
Supi Core <supi.core@gmail.com>
2019-10-31 21:56:15 +0700

    2019-10-30| create table list to enroll&info class

--- src/views/dashboard/peserta/index.vue
+++ src/views/dashboard/peserta/index.vue
@@ -1,5 +1,45 @@
 <template>
     <div style="padding:30px;">
+
+      <el-dialog align="center" title="Info Detail Jadwal" :visible.sync="dialogFormVisible">
+        <el-form :model="form">
+          <el-table
+            v-loading="listLoading"
+            :data="listData"
+            element-loading-text="Loading"
+            border
+            fit
+            highlight-current-row>
+
+          <el-table-column align="center" label="No" width="50">
+              <template slot-scope="scope">
+                {{ scope.$index+1 }}
+              </template>
+            </el-table-column>
+            <el-table-column label="Materi">
+              <template slot-scope="scope">
+                <!-- {{ scope.row.masterKelas.nama_kelas }} -->
+              </template>
+            </el-table-column>
+            <el-table-column label="Instruktur">
+              <template slot-scope="scope">
+                <!-- {{ scope.row.userDetail.nama_lengkap }} -->
+              </template>
+            </el-table-column>
+            <el-table-column label="Sesi">
+              <template slot-scope="scope">
+                <!-- {{ scope.row.userDetail.nama_lengkap }} -->
+              </template>
+            </el-table-column>
+            <el-table-column label="Durasi per Sesi">
+              <template slot-scope="scope">
+                <!-- {{ scope.row.userDetail.nama_lengkap }} -->
+              </template>
+            </el-table-column>
+            </el-table>
+        </el-form>
+      </el-dialog>
+
     <el-alert :closable="false" title="Dashboard Peserta" />
     <br/>
     <el-row :gutter="12">
@@ -24,6 +64,9 @@
       <el-col >
         <el-card header="Jadwal Kursus" shadow="always">
           <!-- List Data -->
+          <span slot="footer" class="dialog-footer">
+              <el-button type="primary" @click="regData">Enroll</el-button>
+            </span>
           <el-table
             v-loading="listLoading"
             :data="listData"
@@ -37,9 +80,9 @@
                 {{ scope.$index+1 }}
               </template>
             </el-table-column>
-            <el-table-column label="Hari">
+            <el-table-column label="Kelas">
               <template slot-scope="scope">
-                {{ scope.row.day.name }}
+                {{ scope.row.masterKelas.nama_kelas }}
               </template>
             </el-table-column>
             <el-table-column label="Jam">
@@ -67,6 +110,73 @@
         </el-card>
       </el-col>
     </el-row>
+
+  <br />
+  <el-row :gutter="12">
+      <el-col >
+        <el-card header="Kelas Sedang Dibuka" shadow="always">
+          <!-- List Data -->
+          <el-table
+            v-loading="listLoading"
+            :data="listData2"
+            element-loading-text="Loading"
+            border
+            fit
+            highlight-current-row
+          >
+            <el-table-column align="center" label="No" width="50">
+              <template slot-scope="scope">
+                {{ scope.$index+1 }}
+              </template>
+            </el-table-column>
+            <el-table-column label="Tipe Kelas">
+              <template slot-scope="scope">
+                {{ scope.row.kelasType.name }}
+              </template>
+            </el-table-column>
+            <el-table-column label="Nama Kelas">
+              <template slot-scope="scope">
+                {{ scope.row.masterKelas.nama_kelas }}
+              </template>
+            </el-table-column>
+            <el-table-column label="Pertemuan" align="center">
+              <template slot-scope="scope">
+                {{ scope.row.jumlah_pertemuan }}
+              </template>
+            </el-table-column>
+            <el-table-column label="Biaya">
+              <template slot-scope="scope">
+                {{ scope.row.biaya }}
+              </template>
+            </el-table-column>
+            <el-table-column label="Status" align="center" width="100">
+              <template slot-scope="scope">
+                <el-tag size="mini"> {{ scope.row.status.name }} </el-tag> 
+              </template>
+            </el-table-column>
+            <el-table-column label="Tanggal Mulai">
+              <template slot-scope="scope">
+                {{ scope.row.tanggal_mulai | formatDate}}
+              </template>
+            </el-table-column>
+            <el-table-column label="Peserta" align="center">
+              <template slot-scope="scope">
+                {{ scope.row.target_peserta }}
+              </template>
+            </el-table-column>
+
+            <el-table-column align="center" label="Action" width="180">
+            <template slot-scope="scope">
+              <el-button @click="infoData(scope)" type="info" size="mini">Info</el-button>
+              <el-button @click="enRoll(scope)" size="mini" type="primary">Enroll</el-button>
+            </template>
+          </el-table-column>
+          </el-table>
+          <!-- End of List Data icon="el-icon-edit-outline" --> 
+        </el-card>
+      </el-col>
+    </el-row>
+
     <br>
     <el-row>
       <el-col>
@@ -82,6 +192,9 @@
 <script>
 import axios from 'axios'
 import Kelas from './components/CarouselCard'
+// import Vue2Filters from 'vue2-filters'
+
+// Vue.use(Vue2Filters)
 
 export default {
     name: 'DashboardPeserta',
@@ -90,16 +203,24 @@ export default {
     },
     created() {
       this.getData()
+      this.getKelas()
     },
     data() {
       return {
         auth: {},
         selectedKelas: '',
         listData: [],
-        listLoading: false
+        listData2: [],
+        listLoading: false,
+        dialogFormVisible: false,
+    //    formLabelWidth: '150px'
       }
     },
     methods: {
+      formatPrice(value) {
+        let val = (value/1).toFixed(2).replace('.', ',')
+        return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+    },
       getData() {
         const token = 'Bearer '+localStorage.getItem('token')
         const auth = {
@@ -114,7 +235,27 @@ export default {
           this.listData = response.data.data;
           this.listLoading = false
         })
-      }      
+      },
+
+      getKelas() {
+        const token = 'Bearer '+localStorage.getItem('token')
+        const auth = {
+          'Authorization' : token,
+          'Content-Type' : 'application/json'
+      }
+      this.listLoading = true
+      axios.get(process.env.VUE_APP_BASE_API + '/kelas/enroll', {headers: auth})
+      .then((response) => {
+        this.listData2 = response.data.data;
+        this.listLoading = false
+      })
+    },
+
+    infoData(scope){
+      this.dialogFormVisible = true 
+      this.form.id = scope.row.id;
+    },
+
     }
 }
 </script>
